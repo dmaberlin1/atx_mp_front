355cfcf81da57e92842086e7aea928977bb8bb66
dmaberlin1 <dmaberlin77@gmail.com>
2023-08-30 11:05:55 +0300

    feat:start task

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import React, {FC} from 'react';
+
+const HomePage:FC = () => {
+    return (
+        <>
+            <h1>main atx Marketplace</h1>
+        </>
+    );
+};
+
+export default HomePage;
